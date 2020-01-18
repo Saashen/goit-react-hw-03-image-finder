79a2b2875072946abead7fc07cf8320be3ae062c
Saashen <saashen.kyiv@gmail.com>
2020-01-18 21:57:21 +0200

    Updates

---
+++ precache-manifest.216c2d7a693f72d3b1370900a9648a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769c189ceb16cf4a85c4b6f4fd3dfc42",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "967c69958ceab0765da7",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.bad2817c.chunk.css"
+  },
+  {
+    "revision": "2505b2c12fe803760e36",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.62cc7144.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.62cc7144.chunk.js.LICENSE"
+  },
+  {
+    "revision": "967c69958ceab0765da7",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.3f811f99.chunk.js"
+  },
+  {
+    "revision": "13285e73bee095c5d415",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.22254703.js"
+  }
+]);
